9508098d678133030a81fb4a9c841806729391f2
canasmh <canasmh@yahoo.com>
2022-12-01 11:31:20 -0500

    Specify which reducer to use

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -33,7 +33,7 @@ const MovieList = (props)=> {
 }
 
 const mapStateToProps = (state) => {
-    return {movies: state.movies}
+    return {movies: state.movieReducer.movies}
 }
 
 export default connect(mapStateToProps, {})(MovieList);
